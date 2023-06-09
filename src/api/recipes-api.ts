65618372e5f639489d4b97ba6096b1045317ac3c
Shukhrat Abdullaev <shukhrat.abdullaev@1pt.com>
2023-06-09 18:04:42 +0500

    change header and refactor edit page

--- src/api/recipes-api.ts
+++ src/api/recipes-api.ts
@@ -17,7 +17,7 @@ export const recipesAPI = {
       },
       {
         id: 2,
-        title: 'Hot-dogs',
+        title: 'Hot-dog',
         url: 'https://www.cnet.com/a/img/resize/69256d2623afcbaa911f08edc45fb2d3f6a8e172/hub/2023/02/03/afedd3ee-671d-4189-bf39-4f312248fb27/gettyimages-1042132904.jpg?auto=webp&fit=crop&height=675&width=1200',
         description: 'A hot dog (commonly spelled hotdog) is a food consisting of a grilled or steamed sausage served in the slit of a partially sliced bun. The term hot dog can refer to the sausage itself. The sausage used is a wiener (Vienna sausage) or a frankfurter (Frankfurter Würstchen, also just called frank).',
       },
