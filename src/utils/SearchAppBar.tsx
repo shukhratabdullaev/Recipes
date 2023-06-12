d9cc6628921efa03ca1c7105eb43e9092d5f4c4b
Shukhrat Abdullaev <shukhrat.abdullaev@1pt.com>
2023-06-12 17:28:14 +0500

    refactoring

--- src/utils/SearchAppBar.tsx
+++ src/utils/SearchAppBar.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react'
-import { styled, alpha } from '@mui/material/styles'
+import { ChangeEvent } from 'react'
+import { alpha, styled } from '@mui/material/styles'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
-import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
 import InputBase from '@mui/material/InputBase'
-import MenuIcon from '@mui/icons-material/Menu'
 import SearchIcon from '@mui/icons-material/Search'
 import { Link } from 'react-router-dom'
 import { useAppDispatch } from '../modules/redux/store'
 import { getSortValue } from '../modules/redux/recipes-reducer'
-import { ChangeEvent } from 'react'
+import Container from '@mui/material/Container/Container'
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -61,35 +60,28 @@ export const SearchAppBar = () => {
   return <Box sx={{ flexGrow: 1 }}>
     <AppBar position='static'>
       <Toolbar>
-        <IconButton
-          size='large'
-          edge='start'
-          color='inherit'
-          aria-label='open drawer'
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography
-          variant='h6'
-          noWrap
-          component='div'
-          sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-        >
-          <Link to='/' style={{ textDecoration: 'none', color: '#fff' }}>
-            Recipes
-          </Link>
-        </Typography>
-        <Search>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase
-            placeholder='Search…'
-            inputProps={{ 'aria-label': 'search' }}
-            onChange={(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => dispatch(getSortValue(event.target.value))}
-          />
-        </Search>
+        <Container fixed sx={{display: 'flex' }}>
+          <Typography
+            variant='h6'
+            noWrap
+            component='div'
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            <Link to='/' style={{ textDecoration: 'none', color: '#fff' }}>
+              Recipes
+            </Link>
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder='Search…'
+              inputProps={{ 'aria-label': 'search' }}
+              onChange={(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => dispatch(getSortValue(event.target.value))}
+            />
+          </Search>
+        </Container>
       </Toolbar>
     </AppBar>
   </Box>
